168d55844d4be8097efeaeffeac3dc1ae7c5d167
Ruslan Shevchenko <runya_@ukr.net>
2024-09-22 13:59:52 +0300

    added task-3 file

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+  const numContent = Number.parseFloat(content);
+  const numPadding = Number.parseFloat(padding);
+  const numBorder = Number.parseFloat(border);
+  return numContent + numPadding * 2 + numBorder * 2;
+}
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
